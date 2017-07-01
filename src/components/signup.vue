ba4dd0823a35e7c5415bf51cb9673cff9b53fba1
Alexis Tyler <xo@wvvw.me>
2017-07-01 19:01:58 +0930

    update signup form

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,34 +1,59 @@
 <template>
-    <div class="signin-page">
-        <div class="container">
-            <form class="form-signin" action="/signin" method="post">
-                <h2 class="form-signin-heading">Signin</h2>
-                <div class="signin-wrap">
-                    <input class="form-control" type="text" placeholder="Username" name="username" autofocus/>
-                    <br>
-                    <input class="form-control" type="password" placeholder="Password" name="password"/>
-                    <label class="checkbox">
-                        <span class="pull-right">
-                            <a href="/signin#myModal">Forgot Password?</a>
-                        </span>
-                    </label>
-                    <button class="btn btn-theme btn-block" href="/signin" type="submit">
-                        <i class="fa fa-lock"></i> SIGN IN
-                    </button>
-                    <hr>
-                    <div class="registration">
-                        Don't have an account yet?
-                        <br>
-                        <a href="/signup">Create an account</a>
-                    </div>
-                </div>
-            </form>
-        </div>
+    <div class="form-signin">
+        <h2 class="form-signin-heading">Signup</h2>
+        <form v-on:submit.prevent="trySignup()" class="signin-wrap">
+            <span class="flash-error">{{error}}</span>
+            <input class="form-control" type="text" placeholder="Username" v-model="username" autofocus/>
+            <br>
+            <input class="form-control" type="password" placeholder="Password" v-model="password"/>
+            <button class="btn btn-theme btn-block" @click="trySignup()">
+                <i class="fa fa-lock"></i> Signup
+            </button>
+            <hr>
+            <div class="registration">
+                Already have an account?
+                <br>
+                <router-view :to="{ name: 'signin' }">Signin</a>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+import {mapGetters, mapActions} from 'vuex';
+
 export default {
-    name: 'signup'
+    name: 'signup',
+    data() {
+        return {
+            loading: false,
+            username: '',
+            password: ''
+        };
+    },
+    methods: {
+        ...mapActions([
+            'signup'
+        ]),
+        trySignup() {
+            const vm = this;
+            const {username, password} = vm;
+            vm.signup({username, password}).then(() => {
+                this.$router.push({name: 'home'});
+            });
+        }
+    },
+    computed: {
+        ...mapGetters({
+            error: 'authError',
+            stack: 'authStack'
+        })
+    }
 };
 </script>
+
+<style scoped>
+.form-signin {
+    margin: 0 auto;
+}
+</style>
